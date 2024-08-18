2a5c13c961327315eeb7b4c88a911c0f3e0410a4
Kranthi <kranthimamidisetty@gmail.com>
2024-08-18 20:06:59 +0530

    Remove check for password

--- app.py
+++ app.py
@@ -9,29 +9,6 @@ def load_data():
     df = pd.read_csv("cleaned_lab_reports_no_pii.csv")
     return df
 
-# Function to check password
-def check_password():
-    def password_entered():
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        st.text_input(
-            "Enter password", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        st.text_input(
-            "Enter password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        return True
-
 def main():
     st.title("Lab Reports Analysis")
 
